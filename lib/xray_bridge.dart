7bee1b56720233053bc27998d6e1bc4af0f1daac
ROikkrl0237 <fenekytblockmine@gmail.com>
2026-09-26 00:32:28 +0300

    Keep config fixture generator independent of Flutter UI

--- lib/xray_bridge.dart
+++ lib/xray_bridge.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:math';
 
-import 'package:vpn_plugin/vpn_plugin.dart';
+import '../packages/vpn_plugin/lib/src/singbox_config.dart';
 
 /// One Xray process per tunnel/probe. SOCKS credentials guard the loopback port.
 class XrayBridge {
